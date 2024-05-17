694993fee6c09c65c3d961ee24dd0090f4aef736
Kunal Garg <kunalgarg054@gmail.com>
2024-05-17 21:23:51 +0530

    schema fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,36 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
             name: "Credentials",
             credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith"},
+                email: { label: "Email", type: "text", placeholder: "jsmith@gmail.com"},
                 password: { label: "Password", type: "password", placeholder: "Password"}
             },
-            async authorize(credentials: Record<"username" | "password", string> | undefined) {
-                const user = { id: "1", name: "Admin" };
-                if (user) {
+            async authorize(credentials: Record<"email" | "password", string> | undefined) {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.email
+                    }
+                });
+
+                if (user && user.password === credentials?.password) {
+                    await prisma.user.create({
+                        data: {
+                            id: user.id,
+                            email: user.email,
+                            password: user.password
+                        }
+                    });
                     return user;
                 } else {
-                    return null;
-                }
+                    throw new Error("Invalid credentials");
+                }           
             }
         })
     ],
